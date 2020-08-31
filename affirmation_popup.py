da961cf65535106ee154b8b01bddc4768ba5e3a7
nina-tan <tangirl2001@hotmail.com>
2020-08-31 12:26:45 -0400

    popup scripts

---
+++ affirmation_popup.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from subprocess import Popen, PIPE
+
+affirmations_list = []
+with open('affirmation.txt') as f:
+    for line in f:
+        affirmations_list.append(line)
+
+# automate popup
+os.system('osascript ./affirmation_notif.scpt "' + affirmations_list[0] + '"')
+
+# prepare next affirmation
+affirmations_list.append(affirmations_list[0])
+affirmations_list.remove(affirmations_list[0])
+with open('affirmation.txt', '+w') as f:
+    for line in affirmations_list:
+        f.write(line)
